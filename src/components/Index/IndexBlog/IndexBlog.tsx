5df924e8407a5feaa980ba302c24034928b2aa50
Jireh June Nimes <jjnimes@gmail.com>
2025-05-29 23:04:47 +0800

    Updating index blog cards

--- src/components/Index/IndexBlog/IndexBlog.tsx
+++ src/components/Index/IndexBlog/IndexBlog.tsx
@@ -1,10 +1,19 @@
 import IndexSectionTitle from '@/components/Index/IndexSectionTitle';
+import IndexBlogCard from './IndexBlogCard';
 import styles from './index-blog.module.css';
 
 export default function IndexBlog() {
+  const blogs = [{}, {}, {}, {}];
+
+  const blogsList = blogs.map((blog, index: number) => (
+    <IndexBlogCard key={index} />
+  ));
+
   return (
-    <section id="blog" className={styles['index-blog']}>
+    <section id="blog" className={styles.section}>
       <IndexSectionTitle>BLOG</IndexSectionTitle>
+
+      <div className={styles.list}>{blogsList}</div>
     </section>
   );
 }
---
+++ src/components/Index/IndexBlog/IndexBlogCard/index.ts
@@ -0,0 +1,3 @@
+import { default as IndexBlogCard } from './IndexBlogCard';
+
+export default IndexBlogCard;
