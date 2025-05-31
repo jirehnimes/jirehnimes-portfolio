d1e09fcfce8d2019f89d7e0b3a978d4bdd236c62
Jireh June Nimes <jjnimes@gmail.com>
2025-06-01 01:08:18 +0800

    Updated chatbot icon and button

--- src/components/ChatBot/ChatBot.tsx
+++ src/components/ChatBot/ChatBot.tsx
@@ -1,12 +1,17 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMessage } from '@fortawesome/free-solid-svg-icons';
+'use client';
+
 import styles from './chat-bot.module.css';
 
 export default function ChatBot() {
   return (
     <div className={styles.container}>
-      <button className={styles.button}>
-        <FontAwesomeIcon icon={faMessage} />
+      <button
+        className={styles.button}
+        onClick={() => window.alert('Coming soon...')}
+      >
+        <svg viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
+          <path d="M832 83H166q-34 0-58.5 24.5T83 166v749l167-166h582q34 0 58.5-24.5T915 665V166q0-34-24.5-58.5T832 83zM250 374h499v83H250v-83zm332 208H250v-83h332v83zm167-249H250v-83h499v83z" />
+        </svg>
       </button>
     </div>
   );
