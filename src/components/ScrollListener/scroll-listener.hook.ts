1ab8d1f3203a4dd6251332c27ae8e5b16d1fac03
Jireh June Nimes <jjnimes@gmail.com>
2025-06-01 22:24:25 +0800

    chore: Updates on UI in Index page (#12)
    * Fix errors
    
    * Compute scroll position on load
    
    * Updated social media list, chatbot, and scroll to top
    
    * Added @trivago/prettier-plugin-sort-imports

--- src/components/ScrollListener/scroll-listener.hook.ts
+++ src/components/ScrollListener/scroll-listener.hook.ts
@@ -7,27 +7,40 @@ import { scrollPositionAtom } from '@/stores/global.store';
 export const useScrollListenerHook = () => {
   const setScrollPosition = useSetAtom(scrollPositionAtom);
 
-  const handleScroll = useCallback(
-    (event: Event) => {
-      const scrollElement = (event?.target as Document)?.scrollingElement;
+  const computeScrollPosition = useCallback(
+    (targetDOM: Document) => {
+      try {
+        const scrollElement = targetDOM?.scrollingElement;
 
-      if (!!scrollElement === true) {
-        const { scrollTop, scrollHeight, clientHeight } = scrollElement;
+        if (!!scrollElement === true) {
+          const { scrollTop, scrollHeight, clientHeight } = scrollElement;
 
-        const position = Math.ceil(
-          (scrollTop / (scrollHeight - clientHeight)) * 100
-        );
+          const position = Math.ceil(
+            (scrollTop / (scrollHeight - clientHeight)) * 100
+          );
 
-        console.log('Scroll position', position);
-        setScrollPosition(position);
-      } else {
-        console.error("Scrolling element can't be detected.");
+          console.log('Scroll position', position);
+          setScrollPosition(position);
+        } else {
+          console.error("Scrolling element can't be detected.");
+        }
+      } catch (error) {
+        console.error(error);
       }
     },
     [setScrollPosition]
   );
 
+  const handleScroll = useCallback(
+    (event: Event) => {
+      computeScrollPosition(event?.target as Document);
+    },
+    [computeScrollPosition]
+  );
+
   useEffect(() => {
+    computeScrollPosition(document);
+
     window.addEventListener('scroll', handleScroll);
-  }, [handleScroll]);
+  }, [handleScroll, computeScrollPosition]);
 };
--- src/types/social-media.type.ts
+++ src/types/social-media.type.ts
@@ -1,6 +1,8 @@
 import { SOCIAL_MEDIA } from '@/constants/global.constants';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
 export type TSocialMedia = {
   name: SOCIAL_MEDIA;
   url: string;
+  icon: IconDefinition;
 };
