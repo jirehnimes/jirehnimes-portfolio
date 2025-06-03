1fd5e14d9fc36ac5c4ec83aa0e32e86399c25e9a
Jireh June Nimes <jjnimes@gmail.com>
2025-06-04 01:01:55 +0800

    chore: Updates on UI in Index page  (#13)
    * Fix errors
    
    * Compute scroll position on load
    
    * Updated social media list, chatbot, and scroll to top
    
    * Added @trivago/prettier-plugin-sort-imports
    
    * Updated social media list
    
    * Fix on waves divider overflow
    
    * Updated social media list
    
    * Fix on waves divider overflow

--- src/components/WavesDivider/WavesDivider.tsx
+++ src/components/WavesDivider/WavesDivider.tsx
@@ -7,30 +7,32 @@ export default function WavesDivider() {
   const { WAVE_COLOR, componentRef, wavePaths } = useWavesDividerHook();
 
   return (
-    <svg
-      ref={componentRef}
-      className={styles.waves}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 1440 320"
-    >
-      <path
-        id="path1"
-        fill={WAVE_COLOR}
-        fillOpacity="1"
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d={wavePaths[0]}
-      ></path>
+    <div className={styles.container}>
+      <svg
+        ref={componentRef}
+        className={styles.waves}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+      >
+        <path
+          id="path1"
+          fill={WAVE_COLOR}
+          fillOpacity="1"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d={wavePaths[0]}
+        ></path>
 
-      <path
-        id="path2"
-        fill={WAVE_COLOR}
-        fillOpacity="1"
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d={wavePaths[1]}
-        style={{ opacity: 0 }}
-      ></path>
-    </svg>
+        <path
+          id="path2"
+          fill={WAVE_COLOR}
+          fillOpacity="1"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d={wavePaths[1]}
+          style={{ opacity: 0 }}
+        ></path>
+      </svg>
+    </div>
   );
 }
