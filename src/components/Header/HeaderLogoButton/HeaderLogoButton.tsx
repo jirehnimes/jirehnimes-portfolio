f87731d3d9c6d4be14e8d309277d165728367442
Jireh June Nimes <jjnimes@gmail.com>
2025-06-08 01:29:31 +0800

    Added icons

--- src/components/Header/HeaderLogoButton/HeaderLogoButton.tsx
+++ src/components/Header/HeaderLogoButton/HeaderLogoButton.tsx
@@ -1,7 +1,12 @@
 'use client';
 
+import Image from 'next/image';
 import styles from './header-logo-button.module.css';
 
 export default function HeaderLogoButton() {
-  return <button className={styles['logo-container']}></button>;
+  return (
+    <button className={styles.button}>
+      <Image src="/icons/icon.png" alt="Jireh Nimes" width={52} height={52} />
+    </button>
+  );
 }
