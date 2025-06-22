7a52e57d1f354e04af4b1bfe5956a517b81a2595
Jireh June Nimes <jjnimes@gmail.com>
2025-06-22 23:57:15 +0800

    Fixed deployment errors

--- src/hooks/home.hook.ts
+++ src/hooks/home.hook.ts
@@ -1,21 +1,3 @@
-import { THEME_MODE } from '@/constants/global.constants';
-import { useEffect, useState } from 'react';
-import useThemeHook from './theme.hook';
-
 export default function useHomeHook() {
-  const { themeMode, toggleThemeMode } = useThemeHook();
-  const [themeText, setThemeText] = useState<THEME_MODE>(THEME_MODE.LIGHT);
-
-  useEffect(() => {
-    if (themeMode === true) {
-      setThemeText(THEME_MODE.DARK);
-    } else {
-      setThemeText(THEME_MODE.LIGHT);
-    }
-  }, [themeMode]);
-
-  return {
-    themeText,
-    toggleThemeMode,
-  };
+  return {};
 }
