ee52209f1e351e5ff5f43d2423cdd53309923cfe
Jireh June Nimes <jjnimes@gmail.com>
2025-07-03 00:14:12 +0800

    Removed backup files and updated about description

--- src/components/Index/IndexAbout/IndexAboutDescription/IndexAboutDescription.tsx
+++ src/components/Index/IndexAbout/IndexAboutDescription/IndexAboutDescription.tsx
@@ -2,10 +2,15 @@ import styles from './index-about-description.module.css';
 
 export default function IndexAboutDescription() {
   return (
-    <div id="about" className={styles['index-about-description']}>
+    <div id="about" className={styles.container}>
       <h4>
-        I’M A FULL-STACK DEVELOPER FROM THE PHILIPPINES WITH ALMOST 9 YEARS OF
-        PROFESSIONAL WORKING EXPERIENCE.
+        I&apos;m an innovative and passionate software developer with a
+        professional experience of almost 9 years in various fields and eager to
+        provide and deliver solutions and outputs that will be valuable and
+        beneficial to the organization. I&apos;m flexible to work in a
+        culturally diverse and collaborative environment. I&apos;m also learning
+        continuously to adapt to the fast development in technology and in
+        business.
       </h4>
     </div>
   );
