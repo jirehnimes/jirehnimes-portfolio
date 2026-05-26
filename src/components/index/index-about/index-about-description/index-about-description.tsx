6f6a992f7c1cdaccda07ebd5b8ec486d742095c7
Jireh Nimes <jjnimes@gmail.com>
2026-05-27 01:04:05 +0800

    feat: Updated IndexAboutDescription component

--- src/components/index/index-about/index-about-description/index-about-description.tsx
+++ src/components/index/index-about/index-about-description/index-about-description.tsx
@@ -1,16 +1,24 @@
+import { formatClasses } from '@/utils/common.util';
 import styles from './index-about-description.module.css';
 
 export const IndexAboutDescription = () => {
   return (
-    <div id="about" className={styles.container}>
+    <div
+      id="about"
+      className={formatClasses([styles.container, 'margin-auto'])}
+    >
       <h4>
-        I&apos;m an innovative and passionate software developer with a
-        professional experience of almost 9 years in various fields and eager to
-        provide and deliver solutions and outputs that will be valuable and
-        beneficial to the organization. I&apos;m flexible to work in a
-        culturally diverse and collaborative environment. I&apos;m also learning
-        continuously to adapt to the fast development in technology and in
-        business.
+        Results-driven software developer with nearly 9+ years of professional
+        experience delivering scalable and impactful solutions across multiple
+        industries. Passionate about building efficient, user-focused
+        applications that create business value and improve user experience.
+        <br />
+        <br />
+        Experienced in working within culturally diverse and collaborative
+        environments, with a strong ability to adapt to evolving technologies
+        and business needs. Committed to continuous learning, innovation, and
+        delivering high-quality solutions that contribute to organizational
+        success.
       </h4>
     </div>
   );
