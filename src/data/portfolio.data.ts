598947fb23b09b2405a5782ba58889833a769aed
Jireh Nimes <jjnimes@gmail.com>
2026-06-04 03:21:56 +0800

    fix: Fixed personal portfolio link

--- src/data/portfolio.data.ts
+++ src/data/portfolio.data.ts
@@ -5,7 +5,7 @@ export const portfolios: TPortfolio[] = [
   {
     name: 'Personal Portfolio',
     icon: '/icons/icon.png',
-    url: '//localhost:3000',
+    url: 'https://www.jirehnimes.com',
     description:
       'A personal portfolio website showcasing my projects and skills.',
     type: PORTFOLIO_TYPE.WEB,
